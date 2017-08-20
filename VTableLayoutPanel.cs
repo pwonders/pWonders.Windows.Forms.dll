d37c08301ca15113e2c8b1e3a3990b3ebd2b2d09
pwonders <me@plainwonders.net>
2017-08-19 21:04:39 -0700

    added tablelayoutpanel to fix transparency issue. added (incomplete) textbox for use on glass (blur-behind form).

---
+++ VTableLayoutPanel.cs
@@ -0,0 +1,62 @@
+﻿using System.ComponentModel;
+using System.Drawing;
+using Microsoft.Win32;
+
+namespace System.Windows.Forms
+{
+	[System.ComponentModel.DesignerCategory("Code")]
+	public class VTableLayoutPanel : TableLayoutPanel
+	{
+		public VTableLayoutPanel()
+		{
+		}
+
+		public override Color BackColor
+		{
+			set
+			{
+				base.BackColor = value;
+				if (value == Color.Transparent)
+				{
+					if (m_FixTransparency)
+					{
+						this.UpdateStyles();
+					}
+				}
+			}
+			get { return base.BackColor; }
+		}
+
+		[DefaultValue(false)]
+		public bool FixTransparency
+		{
+			set
+			{
+				if (m_FixTransparency != value)
+				{
+					m_FixTransparency = value;
+					if (this.BackColor == Color.Transparent)
+					{
+						this.UpdateStyles();
+					}
+				}
+			}
+			get { return m_FixTransparency; }
+		}
+
+		bool m_FixTransparency;
+
+		protected override CreateParams CreateParams
+		{
+			get
+			{
+				CreateParams cp = base.CreateParams;
+				if (m_FixTransparency)
+				{
+					cp.ExStyle |= API.WS_EX_TRANSPARENT;
+				}
+				return cp;
+			}
+		}
+	}
+}
---
+++ VTextBox.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace System.Windows.Forms
+{
+	[System.ComponentModel.DesignerCategory("Code")]
+	public class VTextBox : TextBox
+	{
+		public VTextBox()
+		{
+			SetStyle(ControlStyles.UserPaint, true);
+			SetStyle(ControlStyles.AllPaintingInWmPaint, true);
+		}
+
+		protected override void OnPaint(PaintEventArgs e)
+		{
+			base.OnPaint(e);
+		}
+	}
+}
