ee9a1db20600d1c5682371feee4d909dcd1e4e3b
pwonders <me@plainwonders.net>
2017-09-19 14:43:42 -0700

    added deploy script to post build event. added convenient function to Graphics.

---
+++ Graphics.Extension.cs
@@ -0,0 +1,14 @@
+﻿namespace System.Drawing
+{
+	public static class GraphicsExtension
+	{
+		public static void SetHighQualityMode(this Graphics graphics)
+		{
+			graphics.CompositingQuality = System.Drawing.Drawing2D.CompositingQuality.HighQuality;
+			graphics.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.HighQualityBicubic;
+			graphics.PixelOffsetMode = System.Drawing.Drawing2D.PixelOffsetMode.HighQuality;
+			graphics.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.HighQuality;
+			graphics.TextRenderingHint = System.Drawing.Text.TextRenderingHint.ClearTypeGridFit;
+		}
+	}
+}
